183472f857a812a093cbc4649c66dcced276ca2f
michele camera <michy.cam95@gmail.com>
2022-04-21 17:21:34 +0200

    imported poster img

--- src/components/Filmsection.vue
+++ src/components/Filmsection.vue
@@ -1,7 +1,7 @@
 <template>
   <div div class="card">
-    <img src="" alt="" />
     <div>
+      <img :src="getImgUrl(film)" alt="" />
       <h3>{{ film.title }}</h3>
       <h3>{{ film.original_title }}</h3>
       <h3>{{ film.original_language }}</h3>
@@ -13,21 +13,38 @@
 <script>
 export default {
   name: "Filmsection",
+  data() {
+    return {
+      imgUrl: "",
+    };
+  },
   props: {
     film: Object,
+  },
+  methods: {
+    getImgUrl() {
+      const url = "https://image.tmdb.org/t/p/w200" + this.film.poster_path;
+      return url;
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .card {
+  img {
+    width: 70%;
+    height: 70%;
+  }
+  text-align: center;
   padding: 10px;
   line-height: 20px;
   margin: 2%;
   border: 1px solid black;
-  width: 15%;
+  width: 25%;
   font-size: 0.9rem;
-  height: 250px;
-  background-color: white;
+  color: white;
+  height: 400px;
+  background-color: black;
 }
 </style>
--- src/components/Seriessection.vue
+++ src/components/Seriessection.vue
@@ -1,7 +1,7 @@
 <template>
   <div div class="card">
-    <img src="" alt="" />
     <div>
+      <img :src="getImgUrl(series)" alt="" />
       <h3>{{ series.name }}</h3>
       <h3>{{ series.original_name }}</h3>
       <h3>{{ series.original_language }}</h3>
@@ -16,18 +16,30 @@ export default {
   props: {
     series: Object,
   },
+  methods: {
+    getImgUrl() {
+      const url = "https://image.tmdb.org/t/p/w200" + this.series.poster_path;
+      return url;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .card {
+  img {
+    width: 70%;
+    height: 70%;
+  }
+  text-align: center;
   padding: 10px;
   line-height: 20px;
   margin: 2%;
   border: 1px solid black;
-  width: 15%;
+  width: 25%;
   font-size: 0.9rem;
-  height: 250px;
-  background-color: white;
+  color: white;
+  height: 400px;
+  background-color: black;
 }
 </style>
